ed7daf60a0092871dc11f51e7f7a0f648f9eeea9
Brooks Patton <brooks.patton@galvanize.com>
2016-08-25 13:59:05 -0600

    working on new stuff message

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT;
 let app = express();
 
 app.get('/', (req, res) => {
-  res.json({message: 'I am working!'});
+  res.json({message: 'I am working on new features!!!'});
 });
 
 app.listen(Number(port), () => {
