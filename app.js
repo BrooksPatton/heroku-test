cbcbcbba95d0bd26f019ec3461a3294454643cab
Brooks Patton <brooks.patton@galvanize.com>
2016-08-25 14:18:03 -0600

    added logging

--- app.js
+++ app.js
@@ -3,10 +3,13 @@
 require('dotenv').config();
 
 const express = require('express');
+const morgan = require('morgan');
 
 const port = process.env.PORT;
 
 let app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json({message: 'I am working on new features!!!'});
